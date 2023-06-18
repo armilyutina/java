db0d325daf963847002d76659240bf1caa1f217b
Alina Milyutina <amiliutkina@detmir.ru>
2023-06-18 15:34:59 +0300

    lesson/1: Point class

---
+++ sandbox/src/main/java/ru/qa/Point.java
@@ -0,0 +1,16 @@
+package ru.qa;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p1) {
+        return Math.sqrt(Math.pow(p1.x - this.x, 2) + Math.pow(p1.y - this.y, 2));
+    
+    }
+}
